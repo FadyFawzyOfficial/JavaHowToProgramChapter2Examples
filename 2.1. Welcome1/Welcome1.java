6844c3405745d96a135d1eca64c6d62061f8115e
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2017-01-02 02:00:30 +0200

    2.1. Welcome1

---
+++ 2.1. Welcome1/Welcome1.java
@@ -0,0 +1,9 @@
+// Fig. 2.1: Welcome1.java Author: Eng.Fady Fawzy
+// Text-printing program. Created: 10/8/2016
+
+public class Welcome1 {
+    public static void main(String [] args) {
+        // main method begins execution of Java application
+        System.out.println("Welcome to Java Programming!");
+    } // end method main
+} // end class welcome1
