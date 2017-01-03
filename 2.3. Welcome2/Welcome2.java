c9408b9934220726e7a63710e9a093c9b594f9a1
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2017-01-03 02:00:41 +0200

    2.3. Welcome2

---
+++ 2.3. Welcome2/Welcome2.java
@@ -0,0 +1,10 @@
+// Fig. 2.3: Welcome2.java Author: Eng.Fady Fawzy
+// Print a line of text with multiple statements. Created: 10/8/2016
+
+public class Welcome2 {
+    public static void main(String [] args) {
+        // main method begins execution of Java application
+        System.out.print("Welcome to ");
+        System.out.println("Java Programming!");
+    } // end method main
+} // end class welcome2
