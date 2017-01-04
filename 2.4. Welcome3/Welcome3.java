3440b3579ef44e83eb7aea5adf3ed8433c9b9480
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2017-01-04 02:00:57 +0200

    2.4. Welcome3

---
+++ 2.4. Welcome3/Welcome3.java
@@ -0,0 +1,9 @@
+// Fig. 2.4: Welcome3.java Author: Eng.Fady Fawzy
+// Printing multiple lines of text with a single statement. Created: 10/8/2016
+
+public class Welcome3 {
+    public static void main(String [] args) {
+        // main method begins execution of Java application
+        System.out.println("Welcome\nto\nJava\nProgramming!");
+    } // end method main
+} // end class welcome3
