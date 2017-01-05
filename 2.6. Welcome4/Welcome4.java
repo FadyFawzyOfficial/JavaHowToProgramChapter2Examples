c9065a535afd559769621c0f66a18136af151044
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2017-01-05 02:05:16 +0200

    2.6. Welcome4

---
+++ 2.6. Welcome4/Welcome4.java
@@ -0,0 +1,9 @@
+// Fig. 2.6: Welcome4.java Author: Eng.Fady Fawzy
+// Displaying multiple lines with method System.out.printf. Created: 10/8/2016
+
+public class Welcome4 {
+    public static void main(String [] args) {
+        // main method begins execution of Java application
+        System.out.printf("%s%n%s%n", "Welcome to", "Java Programming!");
+    } // end method main
+} // end class welcome4
